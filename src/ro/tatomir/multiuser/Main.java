ad538eba7dbb06beac4cad30449f3e7f0557bb6f
TATOMIR Radu <Radu.Tatomir@orange.com>
2016-09-15 09:02:58 +0300

    Remove variable conflict.

--- src/ro/tatomir/multiuser/Main.java
+++ src/ro/tatomir/multiuser/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String x = "x";
-		int x = 1;
-        System.out.println("Hello World!");
+	System.out.println("Hello World!");
     }
 }
